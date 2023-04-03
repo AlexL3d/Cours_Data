3bcf29921a0f7d04348effd9c24ebae2effa4208
AlexL3d <alexandre.leduc9@orange.fr>
2023-04-03 16:49:49 +0200

    03/04

---
+++ Exo4/Punition.py
@@ -0,0 +1,16 @@
+#FONCTIONS ============================
+def Ecrire1 () :
+    for i in range (1,101) :
+        print ("Je dois apprendre python pour progresser.")
+
+def Ecrire2 () :
+    cpt = 100
+    while cpt != 0 :
+        print ("Je dois apprendre python pour progresser.")
+        cpt -= 1
+
+#MAIN 
+print ("1ere façon : ")
+Ecrire1()
+print ("2eme façon : ")
+Ecrire2()
