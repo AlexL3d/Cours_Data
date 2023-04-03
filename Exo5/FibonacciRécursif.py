fcce7d57ab80bcac20c851f6ee93d1ce2ae3e698
AlexL3d <alexandre.leduc9@orange.fr>
2023-04-03 16:44:32 +0200

    03/04

--- Exo5/FibonacciRécursif.py
+++ Exo5/FibonacciRécursif.py
@@ -7,11 +7,18 @@ def Saisie() -> int :
         entrée = int(input("Entrez un nombre entier positif non-nul : "))
     return entrée
 
-def Fibonacci () :
+def Fibonacci (itération) :
     
-
-
-
-
+    if itération == 0 or itération == 1 :
+        return 1
+    else :
+        return (Fibonacci(itération-1)+Fibonacci(itération-2))
+  
 #MAIN ================================
 listeFibonacci=[0,1]
+nombreIteration = Saisie()
+
+for index in range (2,nombreIteration) :
+    listeFibonacci.append(Fibonacci(index))
+
+print (listeFibonacci)
