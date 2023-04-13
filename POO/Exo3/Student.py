8319599f791306c9e80681ea49032487fd545724
AlexL3d <alexandre.leduc9@orange.fr>
2023-04-13 11:18:37 +0200

    11/04

--- POO/Exo3/Student.py
+++ POO/Exo3/Student.py
@@ -6,41 +6,49 @@ Le programme demande à l’utilisateur d’entrer le nom et les notes. calc_moy
 moyenne et show() affiche le nom et la note moyenne
 """
 
-class Student :
 
-    def __init__(self) :
+class Student:
+
+    def __init__(self):
         n1, n2, n3 = self.saisie()
         self.set_n1(n1)
         self.set_n2(n2)
         self.set_n3(n3)
-        
-    def saisie(self) :
+
+    def saisie(self):
         n1 = input("Entrez un nom : ")
         n2 = int(input("Entrez un nombre : "))
         n3 = int(input("Entrez un nombre : "))
 
-        return n1,n2,n3
-    
-    def get_n1(self) :
-            return self.__n1
-    
-    def set_n1(self,n1) :
+        return n1, n2, n3
+
+    def get_n1(self):
+        return self.__n1
+
+    def set_n1(self, n1):
         self.__n1 = n1
 
-    def get_n2(self) :
-            return self.__n2
-    
-    def set_n2(self,n2) :
+    def get_n2(self):
+        return self.__n2
+
+    def set_n2(self, n2):
         self.__n2 = n2
-        
-    def get_n3(self) :
+
+    def get_n3(self):
         return self.__n3
-    
-    def set_n3(self,n3) :
+
+    def set_n3(self, n3):
         self.__n3 = n3
-        
-    def moyenne(self) :
+
+    def moyenne(self):
         return (self.__n2 + self.__n3)/2
-        
-    def show(self) :
+
+    def show(self):
         return f"La moyenne de {self.__n1} vaut {self.moyenne()}."
+
+if __name__ == "__main__":
+    try:
+        étudiant = Student()
+        print(étudiant.show())
+    except TypeError as error:
+        print(error)
