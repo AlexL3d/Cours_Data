7ed896a9694eac475c4ee92b9237dbcc029feb5d
AlexL3d <alexandre.leduc9@orange.fr>
2023-04-24 11:10:42 +0200

    Linux Bash

---
+++ Linux/Script.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Exo sur la VM Debian
+
+# Création du dossier Download
+mkdir /home/leduca/Documents/Download
+cd /home/leduca/Documents/Download
+
+# Création des sous-dossiers 
+mkdir Autre
+mkdir Image
+mkdir Music
+mkdir Video
+
+# Création du fichier Readme.txt
+text="Ceci est le fichier de Téléchargement. \nMerci de respecter l'origanisation du répertoire. \nAttention aux virus. \nLe dossier téléchargement est prêt."
+echo -e $text > Readme.txt
+
+#Création du fichier music.mp3
+cd Music
+touch music.mp3
+
