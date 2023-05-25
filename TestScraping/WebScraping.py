c3f7674fb5d2300e2a8616d14fc0dee03d18513f
AlexL3d <alexandre.leduc9@orange.fr>
2023-05-25 13:51:50 +0200

    Projet

--- TestScraping/WebScraping.py
+++ TestScraping/WebScraping.py
@@ -1,23 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-links=[]
+links = []
 url = "https://fr.wikipedia.org/wiki/Liste_des_pays_du_monde"
 
 response = requests.get(url)
 
-if response.ok :
-    soup = BeautifulSoup(response.text, features='html.parser')
+if response.ok:
+    soup = BeautifulSoup(response.text, features="html.parser")
     tds = soup.find_all('td')
-    for td in tds :
+    for td in tds:
         a = td.find("a")
         link = a["href"]
-        links.append('https://fr.wikipedia.org' + link)
+        if ".svg" not in link :
+            links.append("https://fr.wikipedia.org" + link)
 
-with open('TestScraping/url.txt','w') as file :
-    for link in links :
+with open('TestScraping/url.txt', 'w') as file:
+    for link in links:
         file.write(f"{link} \n")
-        
+
 # with open('url.txt','r') as file :
 #     for row in file :
 #         print(row)
