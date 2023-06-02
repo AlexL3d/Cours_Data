398aafa7e38ffb7c7b64a54ec50622a4951c908a
AlexL3d <alexandre.leduc9@orange.fr>
2023-06-02 17:03:20 +0200

    Projet

--- TestScraping/WebScrapingXpath.py
+++ TestScraping/WebScrapingXpath.py
@@ -10,4 +10,4 @@ response = requests.get(url)
 if response.ok :
     soup = BeautifulSoup(response.content, features='html.parser')
     dom = etree.HTML(str(soup))
-    print(dom.xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[3]/td[2]/a')[0].text)
+    print(dom.xpath('//*[@id="mw-content-text"]/div[1]/table[1]/tbody/tr[3]/td[2]/a')[0].attrib)
