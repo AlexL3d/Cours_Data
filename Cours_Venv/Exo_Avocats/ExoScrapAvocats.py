46362d7d9f334b817c23dca2580f8fabc16023ff
AlexL3d <alexandre.leduc9@orange.fr>
2023-06-19 14:32:07 +0200

    Exo Scrap

--- Cours_Venv/Exo_Avocats/ExoScrapAvocats.py
+++ Cours_Venv/Exo_Avocats/ExoScrapAvocats.py
@@ -30,7 +30,7 @@ def ListeAvocats():
     """ Récupération des données d'un site d'avocats
     """
 
-    with open('./Cours_Venv/Exo_Avocats/Liste_Avocats.csv', 'w', newline='') as file:
+    with open('./Cours_Venv/Exo_Avocats/Liste_Avocats.csv', 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         headers = ["Nom et Prenom", "Adresse", "Email", "Telephone"]
         writer.writerow(headers)
@@ -38,41 +38,35 @@ def ListeAvocats():
     for index in range(1, 51):
         MaPage = recupData(index)
         soup = Bs(MaPage, 'html.parser')
-        Face = soup.find('div', class_='facetwp-template')
-        row = Face.find('div', class_='row')
-        Lines = row.find('div', class_="column small-12")
-        Datas = Lines.find_all(
+        Datas = soup.find_all(
             'div', class_="callout secondary annuaire-single")
 
         for Data in Datas:
 
             # Nom et Prénom
-            Nom_Prenom = Data.find('div', class_="avocat-name")
-            Nom_Prenom = Nom_Prenom.find('h3').text.strip()
+            Nom_Prenom = Data.find('h3', class_="nom-prenom").text.strip()
             Nom_Prenom = Nom_Prenom.replace('"', '')
 
             # Adresse
-            Avocat_Adresse = Data.find('div', class_="avocat-adresse")
-            Adresse = Avocat_Adresse.find(
+            Adresse = Data.find(
                 'span', class_="adresse").text.strip()
             Adresse = Adresse.replace('"', '')
             Adresse = Adresse.replace(',', '')
             Adresse = " ".join(Adresse.split())
 
             # Téléphone
-            Téléphone = Avocat_Adresse.find('span', class_="telephone").text.strip()
+            Téléphone = Data.find('span', class_="telephone").text.strip()
             Téléphone = Téléphone[4:]
 
             # Email
-            Avocat_infos = Data.find('div', class_="avocat-infos")
-            Infos = Avocat_infos.find('span', class_="email")
+            Infos = Data.find('span', class_="email")
             try:
                 Email = Infos.find('a').text.strip()
             except AttributeError:
                 Email = "Introuvable"
 
             # Ecriture dans le fichier CSV
-            with open('./Cours_Venv/Exo_Avocats/Liste_Avocats.csv', 'a', newline='') as file:
+            with open('./Cours_Venv/Exo_Avocats/Liste_Avocats.csv', 'a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file, delimiter=',')
                 writer.writerow([Nom_Prenom, Adresse, Email, Téléphone])
 
