1338b4b85cb0017183e587c9759e55d816d7d61d
AlexL3d <alexandre.leduc9@orange.fr>
2023-06-29 16:54:53 +0200

    TDD

--- TDD/ISBN/src/ISBN.py
+++ TDD/ISBN/src/ISBN.py
@@ -1,16 +1,27 @@
 
 
-class IsbnValidator: 
+class IsbnValidator():
 
-    def vérifierIsbn(self, Isbn : int) -> bool:
+    def vérifierIsbn(self, Isbn):
+        if len(Isbn) != 10:
+            raise ValueError("Le code ISBN doit contenir 10 chiffres")
+        elif list(Isbn) in [0,1,2,3,4,5,6,7,8,9,X]:
+            raise ValueError("Le code ISBN doit contenir des chiffres ou X")
+        else:
 
-        total = 0 
+            total = 0
+            Isbn_int = int(Isbn[:9])
 
-        for index in range(10):
-            total += (Isbn%10) * (index+1)
-            Isbn //= 10
+            if Isbn[9] == "X":
+                total += 10
+            else:
+                total += int(Isbn[9])
 
-        if total % 11 == 0:
-            return True
-        else :
-            return False
+            for index in range(1, 10):
+                total += (Isbn_int % 10) * (index+1)
+                Isbn_int //= 10
+
+            if total % 11 == 0:
+                return True
+            else:
+                return False
--- TDD/ISBN/tests/test_ISBN.py
+++ TDD/ISBN/tests/test_ISBN.py
@@ -1,23 +1,33 @@
 # imports
-from src.ISBN import IsbnValidator
+from src.Isbn import IsbnValidator
 import unittest
 
 class test_IsbnValidator(unittest.TestCase):
 
     def test_IsbnValidator_valide(self):
-        IsbnValidator = IsbnValidator()
-        resultat = IsbnValidator.vérifierIsbn(1491926309)
+        Isbn_Validator = IsbnValidator()
+        resultat = Isbn_Validator.vérifierIsbn("1491926309")
         self.assertTrue(resultat,"Le numéro ISBN est valide")
 
     def test_IsbnValidator_invalide(self):
-        IsbnValidator = IsbnValidator()
-        resultat = IsbnValidator.vérifierIsbn(1491926308)
+        Isbn_Validator = IsbnValidator()
+        resultat = Isbn_Validator.vérifierIsbn("1491926308")
         self.assertFalse(resultat,"Le numéro ISBN est invalide")
 
-    def test_IsbnValidator_validebis(self):
-        IsbnValidator = IsbnValidator()
-        resultat = IsbnValidator.vérifierIsbn(2100825208)
+    def test_IsbnValidator_valide_bis(self):
+        Isbn_Validator = IsbnValidator()
+        resultat = Isbn_Validator.vérifierIsbn("2100825208")
         self.assertTrue(resultat,"Le numéro ISBN est valide")
+
+    def test_IsbnValidator_valide_tri(self):
+        Isbn_Validator = IsbnValidator()
+        resultat = Isbn_Validator.vérifierIsbn("043942089X")
+        self.assertTrue(resultat,"Le numéro ISBN est valide")
+
+    def test_IsbnValidator_nombre_Chiffre(self):
+        Isbn_Validator = IsbnValidator()
+        with self.assertRaises(ValueError):
+            Isbn_Validator.vérifierIsbn("149192630")
 
 if __name__ == "__main__":
     unittest.main()
